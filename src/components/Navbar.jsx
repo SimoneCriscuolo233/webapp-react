69fbd312c66419da9c8302c1a010b30673bccf9f
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-09-22 10:30:21 +0200

    fix: correct display logo in navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="container-fluid">
         <div className="col-8">
 
-          <img src="/imgs/logo.png" alt="Logo" className="d-inline-block align-text-top" />
+          <img src="/imgs/logo.png" alt="Logo" className="d-inline-block align-text-center" style={{ maxWidth: "100px", height: "auto" }} />
           <a className="navbar-brand" href="/">
             The Bool Movies
           </a>
